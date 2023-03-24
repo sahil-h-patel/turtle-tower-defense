3531093450d172463a3ff21816f04ea7776a7a26
zaa8026 <zaa8026@rit.edu>
2023-03-24 16:35:40 -0400

    money set up, building set up. things reset properly. working on making crabs walk

---
+++ TurtleTowerDefense/TurtleTowerDefense/BasicCrab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TurtleTowerDefense
+{
+    internal class BasicCrab : Crab
+    {
+        /// <summary>
+        /// Creates a basic, unmodified crab. Bottom of the barrel.
+        /// </summary>
+        /// <param name="image"></param>
+        public BasicCrab(Texture2D image) : base(image)
+        {
+            health = 100;
+            speed = 5;
+            spaceTaken = 1;
+        }
+
+    }
+}
